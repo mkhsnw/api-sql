6bc5ef9615a6abf862220901da7bfd278c5d97a6
mkhsnw <stsunayoshi977@gmail.com>
2024-07-13 00:02:58 +0700

    menambahkan order dan checkout

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from "dotenv"
-import {Sequelize} from "sequelize";
-import {DataTypes} from "sequelize";
-import pg from 'pg'
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+import { DataTypes } from "sequelize";
+import pg from "pg";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
- dialectModule: pg,
+  dialectModule: pg,
 });
 
-try{
+try {
   sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-}catch(error){
-  console.error('Unable to connect to the database:', error);
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
 }
 
-const User = sequelize.define(
-  "User",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    phone_number: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
- 
-);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
 
 const Store = sequelize.define("Store", {
   id: {
@@ -87,53 +83,49 @@ const Store = sequelize.define("Store", {
   },
 });
 
-const Product = sequelize.define(
-  "Product",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    discount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    photo : {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    expiry_date: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    store_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Store,
-        key: "id",
-      },
-      allowNull: false,
-    }
+const Product = sequelize.define("Product", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
-
-);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  discount: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  photo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  expiry_date: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  store_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Store,
+      key: "id",
+    },
+    allowNull: false,
+  },
+});
 
 const Order = sequelize.define("Order", {
   id: {
@@ -209,8 +201,6 @@ const OrderItem = sequelize.define("OrderItem", {
   },
 });
 
-
-
 Store.belongsTo(User, { as: "owner", foreignKey: "ownerId" });
 User.hasOne(Store, { as: "store", foreignKey: "ownerId" });
 
@@ -224,49 +214,49 @@ sequelize.sync({ alter: true }).then(() => {
   console.log("Database & tables created!");
 });
 
-app.get("/", (req,res) => {
-  res.send("Welcome to the server")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
 
-app.get("/user", async (req,res) => {
+app.get("/user", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
-})
+});
 
-app.get('/user/:id', async (req,res) => {
+app.get("/user/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
   res.json(user);
-})
+});
 
-app.get("/toko/:id", async (req,res) => {
+app.get("/toko/:id", async (req, res) => {
   const store = await Store.findByPk(req.params.id);
   res.json(store);
-})
+});
 
-app.post("/login", async (req,res) => {
-  const {email, password} = req.body;
-  const user = await User.findOne({where: {email,password}});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email, password } });
   res.json(user);
-})
+});
 
-app.post('/register', async (req,res) => {
-  const {name, email, password} = req.body;
-  const user = await User.create({name,email,password});
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({ name, email, password });
   res.status(200).json({
     message: "register Berhasiil",
-    data : user
-  })
-})
+    data: user,
+  });
+});
 
-app.patch('/user/:id' , async (req,res) => {
+app.patch("/user/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
-  const {address, phone_number, name,email} = req.body;
-  const update = await user.update({address, phone_number, name,email});
+  const { address, phone_number, name, email } = req.body;
+  const update = await user.update({ address, phone_number, name, email });
   res.status(200).json({
-    message : "berhasil diubah",
-    data : update
-  })
-})
+    message: "berhasil diubah",
+    data: update,
+  });
+});
 
 app.get("/products", async (req, res) => {
   const products = await Product.findAll();
@@ -284,18 +274,18 @@ app.get("/products", async (req, res) => {
   res.status(200).send(productsWithDiscount);
 });
 
-app.get('/product/:id', async (req,res) => {
+app.get("/product/:id", async (req, res) => {
   try {
     const product = await Product.findByPk(req.params.id);
 
     if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
 
     const productData = product.toJSON(); // Konversi ke plain object
 
     if (productData.discount) {
-      productData.discountPrice = 
+      productData.discountPrice =
         productData.price - (productData.price * productData.discount) / 100;
     }
 
@@ -303,111 +293,130 @@ app.get('/product/:id', async (req,res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+});
+
+app.post("/product", async (req, res) => {
+  const { name, description, price, stock, discount, store_id } = req.body;
+  const product = await Product.create({
+    name,
+    description,
+    price,
+    stock,
+    discount,
+    store_id,
   });
-
-  
-
-app.post('/product', async (req,res) => {
-  const {name, description, price, stock, discount, store_id} = req.body;
-  const product = await Product.create({name, description, price, stock, discount,store_id});
   res.status(200).json({
     message: "berhasil ditambahkan",
-    data : product
-  })
-})
+    data: product,
+  });
+});
 
-app.put('/product/:id', async (req,res) => {
+app.put("/product/:id", async (req, res) => {
   const product = await Product.findByPk(req.params.id);
-  const {name, description, price, stock, discount} = req.body;
-  product.update({name, description, price, stock, discount});
-})
+  const { name, description, price, stock, discount } = req.body;
+  product.update({ name, description, price, stock, discount });
+});
 
-app.delete('product/:id', async (req,res) => {
+app.delete("product/:id", async (req, res) => {
   const product = await Product.findByPk(req.params.id);
   product.destroy();
   res.status(200).json({
-    message: "berhasil dihapus"
-  })
-})
-
-app.post("/order", async (req, res) => {
-  const { user_id, items } = req.body;
-  const transaction = await sequelize.transaction();
-
-  try {
-    let total_price = 0;
-    const orderItemsData = [];
-
-    for (const item of items) {
-      const product = await Product.findByPk(item.product_id);
-      if (product.stock < item.quantity) {
-        throw new Error("Stock tidak cukup");
-      }
-      const subtotal = product.price * item.quantity;
-      total_price += subtotal;
-      orderItemsData.push({
-        productId: item.product_id,
-        quantity: item.quantity,
-        price: product.price,
-        subtotal,
-        orderId: null, // This will be set after the order is created
-      });
-    }
-
-    const newOrder = await Order.create(
-      {
-        buyerId: user_id,
-        total_price,
-        status: "PENDING",
-      },
-      { transaction }
-    );
-
-    for (const orderItem of orderItemsData) {
-      orderItem.orderId = newOrder.id;
-    }
-
-    await OrderItem.bulkCreate(orderItemsData, { transaction });
-
-    for (const item of items) {
-      const product = await Product.findByPk(item.product_id);
-      product.stock -= item.quantity;
-      await product.save({ transaction });
-    }
-
-    await transaction.commit();
-    res.status(201).json(newOrder);
-  } catch (error) {
-    await transaction.rollback();
-    res.status(400).json({ message: error.message });
-  }
+    message: "berhasil dihapus",
+  });
 });
 
-app.post('/toko', async (req,res) => {
-  const {name} = req.body
-  const store = await Store.create({name});
+app.post("/order", async (req, res) => {
+ const { user_id, items } = req.body;
+ const transaction = await sequelize.transaction();
+
+ try {
+   let total_price = 0;
+   const orderItemsData = [];
+
+   // Iterasi melalui item yang ada dalam pesanan
+   for (const item of items) {
+     const product = await Product.findByPk(item.product_id);
+     if (!product) {
+       throw new Error(`Product with ID ${item.product_id} not found`);
+     }
+     if (product.stock < item.quantity) {
+       throw new Error(`Stock tidak cukup untuk produk ${product.name}`);
+     }
+     const subtotal = product.price * item.quantity;
+     total_price += subtotal;
+     orderItemsData.push({
+       productId: item.product_id,
+       quantity: item.quantity,
+       price: product.price,
+       subtotal,
+       orderId: null, // Ini akan diisi setelah pesanan dibuat
+     });
+   }
+
+   // Membuat pesanan baru
+   const newOrder = await Order.create(
+     {
+       buyerId: user_id,
+       total_price,
+       status: "PENDING",
+     },
+     { transaction }
+   );
+
+   // Mengaitkan setiap item pesanan dengan pesanan baru
+   for (const orderItem of orderItemsData) {
+     orderItem.orderId = newOrder.id;
+   }
+
+   // Menyimpan item pesanan dalam tabel OrderItem
+   await OrderItem.bulkCreate(orderItemsData, { transaction });
+
+   // Memperbarui stok produk
+   for (const item of items) {
+     const product = await Product.findByPk(item.product_id);
+     product.stock -= item.quantity;
+     await product.save({ transaction });
+   }
+
+   // Commit transaksi
+   await transaction.commit();
+   res.status(201).json({
+     message: "Order created successfully",
+     order: newOrder,
+     orderItems: orderItemsData,
+   });
+ } catch (error) {
+   // Rollback transaksi jika terjadi kesalahan
+   await transaction.rollback();
+   res.status(400).json({ message: error.message });
+ }
+});
+
+app.post("/toko", async (req, res) => {
+  const { name } = req.body;
+  const store = await Store.create({ name });
   res.status(200).json({
     message: "berhasil ditambahkan",
-    data : store
-  })
-})
+    data: store,
+  });
+});
 
-app.get('/product/toko/:id', async (req,res) => {
-  const products = await Product.findAll({where: {store_id: req.params.id}});
+app.get("/product/toko/:id", async (req, res) => {
+  const products = await Product.findAll({
+    where: { store_id: req.params.id },
+  });
   res.status(200).send(products);
-})
+});
 
-app.get('/order/toko/:id', async (req,res) => {
-  const order = await Order.findAll({where: {store_id: req.params.id}});
+app.get("/order/toko/:id", async (req, res) => {
+  const order = await Order.findAll({ where: { store_id: req.params.id } });
   res.status(200).send(order);
-})
+});
 
-app.get('/order/buyer/:id', async (req,res) => {
+app.get("/order/buyer/:id", async (req, res) => {
   const order = await Order.findByPk(req.params.id);
   res.status(200).send(order);
-})
-
-
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
